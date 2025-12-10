07a332e9fc22685d00c67d3b2733b400b06a49db
Baboo81 <magicbaboo@gmail.com>
2025-12-10 13:18:17 +0100

    create table : products

--- database/migrations/2025_12_10_115103_create_products_table.php
+++ database/migrations/2025_12_10_115103_create_products_table.php
@@ -11,8 +11,13 @@ return new class extends Migration
      */
     public function up(): void
     {
+        //Création de la table (Blueprint $table est un objet qui permet de décrire les colonnes de la table)
         Schema::create('products', function (Blueprint $table) {
             $table->id();
+            $table->string('name');
+            $table->text('description')->nullable();
+            $table->decimal('price', 8, 2);//8 = nb total de chiffres et 2 = nb de chiffres après la virgule
+            $table->string('image')->nullable();
             $table->timestamps();
         });
     }
