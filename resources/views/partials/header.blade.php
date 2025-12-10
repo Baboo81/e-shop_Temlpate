a88f05dff98f6f4f852b9d9d6970f799e080b6ef
Baboo81 <magicbaboo@gmail.com>
2025-12-10 15:57:49 +0100

    home.blade.php

--- resources/views/partials/header.blade.php
+++ resources/views/partials/header.blade.php
@@ -1,14 +1,4 @@
 {{-- partials/header.blade.php --}}
 <header class="bg-white shadow-sm">
-    <div class="container py-3 d-flex justify-content-between align-items-center">
-        {{-- Logo --}}
-        <a href="{{ route('home') }}" class="navbar-brand">
-            <img src="{{ asset('assets/images/logo.png') }}" alt="Logo" height="50">
-        </a>
 
-        {{-- Message / Bandeau optionnel --}}
-        <div class="header-text">
-            <p class="mb-0">Bienvenue sur notre mini-shop !</p>
-        </div>
-    </div>
 </header>
