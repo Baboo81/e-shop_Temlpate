538cf9d957af93238b3f1eaea857e33866ce305b
Baboo81 <magicbaboo@gmail.com>
2025-12-10 16:46:12 +0100

    data

--- database/migrations/2025_12_10_125315_create_orders_table.php
+++ database/migrations/2025_12_10_125315_create_orders_table.php
@@ -13,6 +13,9 @@ return new class extends Migration
     {
         Schema::create('orders', function (Blueprint $table) {
             $table->id();
+            $table->foreignId('user_id')->constrained()->onDelete('cascade');//Relie la commande à un utilisateur connecté
+            $table->json('items');//Stocke le panier complet
+            $table->decimal('total', 8, 2);
             $table->timestamps();
         });
     }
